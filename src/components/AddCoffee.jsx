b3f9382221d82d0574a7b0be1cd79f125c6277b7
jihaduddinjbm <jihaduddin117@gmail.com>
2023-11-17 12:28:19 +0600

    added a addCoffee and updateCoffee file

---
+++ src/components/AddCoffee.jsx
@@ -0,0 +1,11 @@
+
+
+const AddCoffee = () => {
+    return (
+        <div>
+            <h2> Add Coffee is Here man </h2>
+        </div>
+    );
+};
+
+export default AddCoffee;
---
+++ src/components/UpdateCoffee.jsx
@@ -0,0 +1,11 @@
+
+
+const UpdateCoffee = () => {
+    return (
+        <div>
+            <h2> Add a coffee for update</h2>
+        </div>
+    );
+};
+
+export default UpdateCoffee;
